71dc640f77e923d2d5be4cc7d707190ac73422c2
Chris Barbour <chris.barbour03@gmail.com>
2020-03-26 09:16:56 +0000

    deploying billing bucket

--- stack/Stack.kt
+++ stack/Stack.kt
@@ -65,21 +65,14 @@ class IamStack: StackBuilder {
 class BillingBucketStack: StackBuilder {
     override fun KloudFormation.create(args: List<String>) {
         this.description = "This Stack will create a bucket that can be used to store your billing reports"
-        val bucketName = parameter<String>("BucketName").ref()
+        val bucketName = parameter<String>("BucketName", default = "klouds-billing-reports").ref()
         val kloudsAccount = parameter<String>("KloudsAccount", default = "662158168835")
-        val bucket = bucket {
-            bucketName(bucketName)
-        }
+        val bucket = bucket { bucketName(bucketName) }
         bucketPolicy(bucket.Arn(), policyDocument {
             statement(
-                    action = actions("s3:GetBucketLocation", "s3:ListBucket"),
-                    resource = resource(bucket.Arn())
-            ) {
-                principal(PrincipalType.AWS, listOf(+"arn:aws:iam::" + kloudsAccount.ref() + ":root"))
-            }
-            statement(
-                    action = action("s3:GetObject"),
-                    resource = resource(bucket.Arn() + "/*")
+                    sid = "KloudsAccessToBucket",
+                    action = actions("s3:GetBucketLocation", "s3:ListBucket", "s3:GetObject"),
+                    resource = resources(bucket.Arn(), bucket.Arn() + "/*")
             ) {
                 principal(PrincipalType.AWS, listOf(+"arn:aws:iam::" + kloudsAccount.ref() + ":root"))
             }
