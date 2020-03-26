fd388f4c8c899f7be1fb985caa594dc7db20fa11
Chris Barbour <chris.barbour03@gmail.com>
2020-03-26 08:55:38 +0000

    added billing bucket stack

--- stack/Stack.kt
+++ stack/Stack.kt
@@ -1,11 +1,17 @@
 import io.hexlabs.kloudformation.module.s3.s3Website
 import io.kloudformation.KloudFormation
 import io.kloudformation.StackBuilder
+import io.kloudformation.function.plus
+import io.kloudformation.model.iam.PrincipalType
 import io.kloudformation.model.iam.action
+import io.kloudformation.model.iam.actions
 import io.kloudformation.model.iam.policyDocument
+import io.kloudformation.model.iam.resource
 import io.kloudformation.model.iam.resources
 import io.kloudformation.property.aws.iam.user.Policy
 import io.kloudformation.resource.aws.iam.user
+import io.kloudformation.resource.aws.s3.bucket
+import io.kloudformation.resource.aws.s3.bucketPolicy
 import io.kloudformation.unaryPlus
 
 class BucketStack: StackBuilder {
@@ -13,6 +19,15 @@ class BucketStack: StackBuilder {
         s3Website {
             s3Bucket {
                 bucketName("klouds-user-template")
+                websiteConfiguration {
+                    indexDocument("template.yml")
+                    errorDocument("template.yml")
+                }
+            }
+        }
+        s3Website {
+            s3Bucket {
+                bucketName("klouds-billing-bucket-template")
                 websiteConfiguration {
                     indexDocument("template.yml")
                     errorDocument("template.yml")
@@ -44,5 +59,32 @@ class IamStack: StackBuilder {
                 }
             )))
         }
+    }
+}
+
+class BillingBucketStack: StackBuilder {
+    override fun KloudFormation.create(args: List<String>) {
+        this.description = "This Stack will create a bucket that can be used to store your billing reports"
+        val bucketName = parameter<String>("BucketName").ref()
+        val kloudsAccount = parameter<String>("KloudsAccount", default = "662158168835")
+        val bucket = bucket {
+            bucketName(bucketName)
+        }
+        bucketPolicy(bucket.Arn(), policyDocument {
+            statement(
+                    sid = "Klouds Access to Bucket",
+                    action = actions("s3:GetBucketLocation", "s3:ListBucket"),
+                    resource = resource(bucket.Arn())
+            ) {
+                principal(PrincipalType.AWS, listOf(+"arn:aws:iam::" + kloudsAccount.ref() + ":root"))
+            }
+            statement(
+                    sid = "Klouds Access to Bucket Contents",
+                    action = action("s3:GetObject"),
+                    resource = resource(bucket.Arn() + "/*")
+            ) {
+                principal(PrincipalType.AWS, listOf(+"arn:aws:iam::" + kloudsAccount.ref() + ":root"))
+            }
+        })
     }
 }
