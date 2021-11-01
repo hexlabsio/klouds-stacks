887dfc7f102b2e0ec49767eb923a6dd223a8d515
Chris Barbour <chris.barbour03@gmail.com>
2021-11-01 19:33:46 +0000

    generating cost reprt

--- stack/generate-cost-reports.js
+++ stack/generate-cost-reports.js
@@ -1,30 +1,44 @@
 const aws = require('aws-sdk');
 const response = require('cfn-response');
-exports.handler = async function(event, context) {
-    try {
-        const params = {
-            ReportDefinition: {
-                "ReportName": "CostReport",
-                "TimeUnit": "DAILY",
-                "Format": "textORcsv",
-                "Compression": "GZIP",
-                "AdditionalSchemaElements": [
-                    "RESOURCES"
-                ],
-                "S3Bucket": "xxxx-testbucket-2",
-                "S3Prefix": "hexlabs",
-                "S3Region": "eu-west-1",
-                "AdditionalArtifacts": [],
-                "RefreshClosedReports": true,
-                "ReportVersioning": "OVERWRITE_REPORT"
-            }
-        }
-        const cur = new aws.CUR();
-        await cur.putReportDefinition(params).promise();
-        response.send(event, context, "SUCCESS", {});
-    } catch(e) {
-        console.error(e);
-        response.send(event, context, "FAILED", {});
-    }
 
+exports.handler = async function(event, context) {
+    if(event.RequestType === 'Create') {
+        try {
+            const properties = event.ResourceProperties;
+            const bucket = properties.Bucket;
+            const region = properties.Region;
+
+            if(!bucket || !region) {
+                console.log('Bucket or Region not provided');
+                response.send(event, context, "FAILED", {});
+                return;
+            }
+            const params = {
+                ReportDefinition: {
+                    "ReportName": "klouds-cost-reports",
+                    "TimeUnit": "DAILY",
+                    "Format": "textORcsv",
+                    "Compression": "GZIP",
+                    "AdditionalSchemaElements": [
+                        "RESOURCES"
+                    ],
+                    "S3Bucket": properties.Bucket,
+                    "S3Prefix": "costs",
+                    "S3Region": properties.Region,
+                    "AdditionalArtifacts": [],
+                    "RefreshClosedReports": true,
+                    "ReportVersioning": "OVERWRITE_REPORT"
+                }
+            }
+            const cur = new aws.CUR();
+            await cur.putReportDefinition(params).promise();
+            response.send(event, context, "SUCCESS", {});
+            return;
+        } catch (e) {
+            console.error(e);
+            response.send(event, context, "FAILED", {});
+            return;
+        }
+    }
+    response.send(event, context, "SUCCESS", {});
 }
--- stack/stack.ts
+++ stack/stack.ts
@@ -1,7 +1,7 @@
 import {Template} from "@hexlabs/kloudformation-ts";
 import {Iam, Policy} from "@hexlabs/kloudformation-ts/dist/kloudformation/iam/PolicyDocument";
 import {Lambda} from "@hexlabs/kloudformation-ts/dist/kloudformation/modules/lambda";
-import {Value} from "@hexlabs/kloudformation-ts/dist/kloudformation/Value";
+import {join, Value} from "@hexlabs/kloudformation-ts/dist/kloudformation/Value";
 import fs from 'fs';
 
 Template.create(aws => {
@@ -20,14 +20,51 @@ export interface ConnectorRequest {
 }
 
 Template.createWithParams({
-  UniqueId: {type: 'String'}
+  UniqueId: {type: 'String'},
+  KloudsUserIdentifier: {type: 'String', description: 'A temporary ID used to refer back to the user that requested this in app, do not change.'},
+  ConnectorPrincipalId: { type: 'String', description: 'The principal that is allowed to assume this role, do not change.' },
+  ConnectorExternalId: { type: 'String', description: 'The external id used to match the connector when assuming this role, do not change.' },
+  ConnectorEndpoint: { type: 'String', description: 'The endpoint to send the role arn to when complete so kloud.io can assume this role, do not change.' }
 }, (aws, params) => {
+  const bucket = aws.s3Bucket({ bucketName: join('klouds-cost-reports-', params.UniqueId()) });
+  const role = aws.iamRole({
+    roleName: join('klouds-connector-', params.UniqueId()),
+    assumeRolePolicyDocument: {
+      statement: [{effect: 'Allow', principal: { AWS: [params.ConnectorPrincipalId()]}, action: 'sts:AssumeRole', condition: { StringEquals: { _nocaps: true, 'sts:ExternalId': params.ConnectorExternalId()}} }]
+    },
+    managedPolicyArns: ['arn:aws:iam::aws:policy/SecurityAudit'],
+    policies: [{
+      policyName: 'APIGatewayGETDomainNamePolicy',
+      policyDocument: {
+        version: '2012-10-17',
+        statement: [{action: 'apigateway:GET', effect: 'Allow', resource: [
+            "arn:aws:apigateway:*::/domainnames",
+            "arn:aws:apigateway:*::/domainnames/*",
+            "arn:aws:apigateway:*::/domainnames/*/basepathmappings",
+            "arn:aws:apigateway:*::/domainnames/*/basepathmappings/*"
+          ]}]
+      }
+    }]
+  });
+  Iam.from(role)
+    .add('CostReportPolicy', Policy.allow(['s3:ListBucket', 's3:GetObject'], bucket.attributes.Arn));
   const lambda = Lambda.create(aws, 'klouds-cost-report-generator',{
     zipFile: fs.readFileSync('stack/generate-cost-reports.js').toString()}, 'index.handler', 'nodejs14.x' );
-  Iam.from(lambda.role).add('CostReportPolicy', Policy.allow(['cur:PutReportDefinition', 'cur:DeleteReportDefinition'], '*'));
-  aws.customResource('KloudsConnector', {
+  Iam.from(lambda.role)
+  .add('CostReportPolicy', Policy.allow(['cur:PutReportDefinition', 'cur:DeleteReportDefinition'], '*'));
+  aws.customResource('KloudsCostReportGenerator', {
     ServiceToken: lambda.lambda.attributes.Arn,
+    Bucket: bucket,
+    Region: { Ref: 'AWS::Region' }
   });
+  aws.customResource<ConnectorRequest>('KloudsConnector', {
+    _dependsOn: [bucket],
+    ServiceToken: params.ConnectorEndpoint(),
+    RoleArn: role.attributes.Arn,
+    UserIdentifier: params.KloudsUserIdentifier()
+  });
+  
+  
 }, 'template/with-cost-reports.json', t => JSON.stringify(({...t, Description: 'An IAM Role to grant READONLY access to klouds.io'})));
 
 Template.createWithParams({
