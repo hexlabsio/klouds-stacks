3023ffd44f3fab519c5812679c3d6b045e0fab8d
Chris Barbour <chris.barbour@hexlabs.io>
2022-11-29 11:14:56 +0000

    removing lambdas in favour of cloudforamtion

--- stack/stack.ts
+++ stack/stack.ts
@@ -2,10 +2,8 @@ import {TemplateBuilder} from "@hexlabs/kloudformation-ts";
 import {Role} from "@hexlabs/kloudformation-ts/dist/aws/iam/Role";
 import {AWS} from "@hexlabs/kloudformation-ts/dist/kloudformation/aws";
 import {Iam, iamPolicy, Policy} from "@hexlabs/kloudformation-ts/dist/kloudformation/iam/PolicyDocument";
-import { Lambda } from '@hexlabs/kloudformation-ts/dist/kloudformation/modules/lambda';
 import {join, Value} from "@hexlabs/kloudformation-ts/dist/kloudformation/Value";
 import crypto from 'crypto';
-import * as fs from 'fs';
 
 TemplateBuilder.create('bucket.json').build(aws => {
   aws.s3Bucket({
@@ -144,15 +142,8 @@ function connectorRole(aws: AWS, uniqueId: Value<string>, principalId: Value<str
   TemplateBuilder
     .create('end-to-end-for-stack-set.json')
     .outputTo('template')
-    .withCondition('RootAccount', { 'Fn::Equals': [{ 'Ref': 'RootAccountId' }, { 'Ref': 'AWS::AccountId' }] })
-    .withCondition('NotRootAccount', { 'Fn::Not': [{ 'Fn::Equals': [{ 'Ref': 'RootAccountId' }, { 'Ref': 'AWS::AccountId' }] }] })
     .params({
       UniqueId: {type: 'String'},
-      ReportBucket: {type: 'String'},
-      ReportBucketArn: {type: 'String'},
-      ReportBucketRegion: {type: 'String'},
-      ReportName: {type: 'String'},
-      RootAccountId: { type: 'String', description: 'If this is the same as the current account then cost and usage reports will be generated' },
       KloudsUserIdentifier: {
         type: 'String',
         description: 'A temporary ID used to refer back to the user that requested this in app, do not change.'
@@ -175,24 +166,17 @@ function connectorRole(aws: AWS, uniqueId: Value<string>, principalId: Value<str
       Description: "Generates Cost and Usage Reports and creates a cross-account IAM Role with READONLY access for use by klouds.io"
     }))
     .build((aws, params, conditional) => {
-      const role = conditional('RootAccount', connectorRole(aws, params.UniqueId(), params.ConnectorPrincipalId(), params.ConnectorExternalId()));
-      Iam.from(role).add('CostReportPolicy', Policy.allow(['s3:ListBucket', 's3:GetObject'], [params.ReportBucketArn(), join(params.ReportBucketArn(), '/*')] as any));
-      const roleWithoutBucket = conditional('NotRootAccount', connectorRole(aws, params.UniqueId(), params.ConnectorPrincipalId(), params.ConnectorExternalId()));
+      const role = conditional('NotRootAccount', connectorRole(aws, params.UniqueId(), params.ConnectorPrincipalId(), params.ConnectorExternalId()));
       aws.customResource<ConnectorRequest>('KloudsConnector', {
         ServiceToken: params.ConnectorEndpoint(),
-        RoleArn: { 'Fn::If': ['RootAccount', role.attributes.Arn, roleWithoutBucket.attributes.Arn] } as any,
+        RoleArn: role.attributes.Arn,
         UserIdentifier: params.KloudsUserIdentifier(),
-        ReportBucket: { 'Fn::If': ['RootAccount', params.ReportBucket(), { Ref: 'AWS::NoValue' }] } as any,
-        ReportBucketRegion: { 'Fn::If': ['RootAccount', params.ReportBucketRegion(), { Ref: 'AWS::NoValue' }] } as any,
-        ReportPrefix: { 'Fn::If': ['RootAccount', 'costs', { Ref: 'AWS::NoValue' }] } as any,
-        ReportName: { 'Fn::If': ['RootAccount', params.ReportName(), { Ref: 'AWS::NoValue' }] } as any,
         StackId: {Ref: 'AWS::StackId'},
         Region: {Ref: 'AWS::Region'},
       });
-
       return {
         outputs: {
-          KloudsConnectorRoleArn: {description: 'Role used by klouds.io to read resources', value: { 'Fn::If': ['RootAccount', role.attributes.Arn, roleWithoutBucket.attributes.Arn] } }
+          KloudsConnectorRoleArn: {description: 'Role used by klouds.io to read resources', value: role.attributes.Arn }
         }
       }
     });
@@ -202,7 +186,7 @@ function connectorRole(aws: AWS, uniqueId: Value<string>, principalId: Value<str
     .create('klouds-stack-set.json')
     .params({
       UniqueId: {type: 'String'},
-      LambdaName: { type: 'String', default: 'klouds-stack-set-creator', description: 'A unique name for a lambda that generates stack sets in your account' },
+      OrganizationalUnitIds: {type: 'CommaDelimitedList', description: 'A comma seperated list of organizational unit ids, you may also provide the root id if you want all accounts'},
       KloudsUserIdentifier: {
         type: 'String',
         description: 'A temporary ID used to refer back to the user that requested this in app, do not change.'
@@ -227,6 +211,7 @@ function connectorRole(aws: AWS, uniqueId: Value<string>, principalId: Value<str
     }))
     .build((aws, params) => {
       const bucket = aws.s3Bucket({bucketName: join('klouds-cost-reports-', params.UniqueId())});
+      bucket._deletionPolicy = 'Retain';
       const bucketPolicy = aws.s3BucketPolicy({
         bucket,
         policyDocument: iamPolicy({
@@ -246,7 +231,7 @@ function connectorRole(aws: AWS, uniqueId: Value<string>, principalId: Value<str
           ]
         })
       });
-      const report = aws.curReportDefinition({
+      aws.curReportDefinition({
         _dependsOn: [bucket._logicalName!, bucketPolicy._logicalName!],
         compression: 'GZIP',
         format: 'textORcsv',
@@ -259,24 +244,39 @@ function connectorRole(aws: AWS, uniqueId: Value<string>, principalId: Value<str
         timeUnit: 'DAILY',
         additionalSchemaElements: ["RESOURCES"]
       });
-      const lambda = Lambda.create(aws, 'klouds-stack-set-creator', {zipFile: fs.readFileSync('stack/stack-set-creator.js').toString()}, 'index.handler', 'nodejs16.x', {
-        functionName: params.LambdaName(),
-        timeout: 900
-      });
-      Iam.from(lambda.role).add('StackSetPolicy', Policy.allow(['cloudformation:*', 'organizations:DescribeOrganization'], '*'));
-
-      aws.customResource('KloudsStackSetGenerator', {
-        ServiceToken: lambda.lambda.attributes.Arn,
-        KloudsUserIdentifier: params.KloudsUserIdentifier(),
-        UniqueId: params.UniqueId(),
-        RootAccountId: {Ref: 'AWS::AccountId'},
+      const role = connectorRole(aws, params.UniqueId(), params.ConnectorPrincipalId(), params.ConnectorExternalId());
+      Iam.from(role).add('CostReportPolicy', Policy.allow(['s3:ListBucket', 's3:GetObject'], [bucket.attributes.Arn, join(bucket.attributes.Arn, '/*')] as any));
+      aws.customResource<ConnectorRequest>('KloudsConnector', {
+        ServiceToken: params.ConnectorEndpoint(),
+        RoleArn: role.attributes.Arn,
+        UserIdentifier: params.KloudsUserIdentifier(),
+        ReportBucket: bucket.attributes.Arn,
+        ReportBucketRegion: {Ref: 'AWS::Region'},
+        ReportPrefix: 'costs',
+        ReportName: {Ref: 'AWS::Region'},
+        StackId: {Ref: 'AWS::StackId'},
         Region: {Ref: 'AWS::Region'},
-        ReportName: {Ref: report._logicalName},
-        ReportBucket: {Ref: bucket._logicalName},
-        ReportBucketArn: bucket.attributes.Arn,
-        ConnectorPrincipalId: params.ConnectorPrincipalId(),
-        ConnectorEndpoint: params.ConnectorEndpoint(),
-        ConnectorExternalId: params.ConnectorExternalId(),
+      });
+      aws.cloudformationStackSet({
+        description: 'Deploys read-only IAM Roles in each account in order to connect to klouds.io',
+        autoDeployment: { enabled: true, retainStacksOnAccountRemoval: false },
+        permissionModel: 'SERVICE_MANAGED',
+        stackSetName: join('klouds-connector', params.UniqueId()),
+        capabilities: ['CAPABILITY_NAMED_IAM'],
+        templateURL: 'https://klouds-user-template.s3.eu-west-1.amazonaws.com/end-to-end-for-stack-set.json',
+        parameters: [
+          { parameterKey: 'UniqueId', parameterValue: params.UniqueId() },
+          { parameterKey: 'KloudsUserIdentifier', parameterValue: params.KloudsUserIdentifier() },
+          { parameterKey: 'ConnectorPrincipalId', parameterValue: params.ConnectorPrincipalId() },
+          { parameterKey: 'ConnectorExternalId', parameterValue: params.ConnectorExternalId() },
+          { parameterKey: 'ConnectorEndpoint', parameterValue: params.ConnectorEndpoint() },
+        ],
+        stackInstancesGroup: [
+          {
+            regions: ['us-east-1'],
+            deploymentTargets: { organizationalUnitIds: params.OrganizationalUnitIds() }
+          }
+        ]
       });
     });
 })();
